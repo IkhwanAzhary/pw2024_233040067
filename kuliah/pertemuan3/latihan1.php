811e6c916100f49c642931e0dad97ad6c1cda3a0
IkhwanAzhary <ikhwanazhary9@gmail.com>
2024-02-25 23:30:38 +0700

    Mengerjakan Tugas 2

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,3 @@
+<?php echo "<h1>Hello World!</h1>"; ?>
+<?php echo "<p>Ikhwan Azhary</p>"; ?>
+<?php echo 7 == 7; ?>
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,6 @@
+<?php 
+$nama = "Ikhwan";
+$umur = "19";
+$kampus = "Universitas Pasundan";
+echo "<b><i><h1>Hello,</h1><br> nama saya $nama umur saya $umur tahun, saya kuliah di $kampus</i></b>";
+?>
