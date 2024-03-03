dae6e3adf33fc25f3459eb935aeb775b33d67bd6
IkhwanAzhary <ikhwanazhary9@gmail.com>
2024-03-03 22:01:49 +0700

    Mengerjakan Tugas 3

--- Tugas/Tugas2/2b.php
+++ Tugas/Tugas2/2b.php
@@ -1,7 +1,20 @@
 <?php
-for ($i = 1; $i <= 10; $I++) {
-    for ($j = 1; $j <= $i; $j++) {
-        echo "$j";
+// for ($i = 1; $i <= $; $I++) {
+//     for ($j = 1; $j <= $i; $j++) {
+//         echo "$j";
+//     }
+//     echo "<br>";
+// }
+
+function cetak_angka($n){
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "$j";
+        }
+        echo "<br>";
     }
-    echo "<br>";
 }
+
+cetak_angka(5);
+echo "<hr>";
+cetak_angka(10);
